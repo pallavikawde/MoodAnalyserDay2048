f1dcce9ef2277c48000064cbbb1cbbd96e118c1e
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-09-03 18:44:18 +0530

    Added:uc1_testcases

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -8,16 +8,13 @@ public class MoodAnalyzer {
     //calling  analyzemood fuction
     public String analyzeMood(String message){
 
-        System.out.println("welcome to moodanlyser");
-     return analyzeMood("welcome");
-//
-////using if else return happy sad
-//        if(message.contains("sad")){
-//            return "SAD";
-//        }
-//        else {
-//            return "HAPPY";
-//        }
+// if else return happy sad
+       if(message.contains("sad")){
+          return "SAD";
+       }
+        else {
+            return "HAPPY";
+        }
    }
 }
 
